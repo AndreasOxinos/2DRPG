d3b0bbfa82db81dc2dbdef9df951d09603bc4b65
AndreasOxinos <oxinosa@hotmail.com>
2013-12-15 18:06:28 +0200

    WallAdded
    Wall Logic added on the scene, now ready to move to diff levels :D

--- 2DRPG/Assets/Scripts/Assassin_Movement.cs
+++ 2DRPG/Assets/Scripts/Assassin_Movement.cs
@@ -3,61 +3,86 @@ using System.Collections;
 
 public class Assassin_Movement : MonoBehaviour
 {
-	public float SpeedX = 1.0f;
-	public float SpeedY = 1.0f;
-	public Animator anim;
-	public string axisNameX = "Horizontal";
-	public string axisNameY = "Vertical";
-	private int previousTrigger;
+    public float SpeedX = 1.0f;
+    public float SpeedY = 1.0f;
+    public Animator anim;
+    public string axisNameX = "Horizontal";
+    public string axisNameY = "Vertical";
+    private int previousTrigger;
 
-	void Start ()
-	{
-		anim = gameObject.GetComponent<Animator> ();
-	}
-	
-	void Update ()
-	{
-		if (IsOnTheGround ()) {
-			CheckMoveX ();
-			CheckMoveY ();
-		}
-	}
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        if (IsOnTheGround())
+        {
+            if (NoWallX())
+            {
+                CheckMoveX();
+            }
+            if (NoWallY())
+            {
+                CheckMoveY();
+            }
+        }
+    }
 
-	void CheckMoveX ()
-	{
-		anim.SetFloat ("SpeedX", Input.GetAxis (axisNameX));
-		if (Input.GetAxis (axisNameX) < 0) {
-			Vector3 newScale = transform.localScale;
-			//newScale.x = -1.0f;
-			transform.localScale = newScale;
-		} else if (Input.GetAxis (axisNameX) > 0) {
-			Vector3 newScale = transform.localScale;
-			//newScale.x = 1.0f;
-			transform.localScale = newScale;
-		}
-		transform.position += transform.right * Input.GetAxis (axisNameX) * SpeedX * Time.deltaTime;
-	}
+    void CheckMoveX()
+    {
+        anim.SetFloat("SpeedX", Input.GetAxis(axisNameX));
+        if (Input.GetAxis(axisNameX) < 0)
+        {
+            Vector3 newScale = transform.localScale;
+            //newScale.x = -1.0f;
+            transform.localScale = newScale;
+        } else if (Input.GetAxis(axisNameX) > 0)
+        {
+            Vector3 newScale = transform.localScale;
+            //newScale.x = 1.0f;
+            transform.localScale = newScale;
+        }
+        transform.position += transform.right * Input.GetAxis(axisNameX) * SpeedX * Time.deltaTime;
+    }
 
-	void CheckMoveY ()
-	{
-		anim.SetFloat ("SpeedY", Input.GetAxis (axisNameY));
-		if (Input.GetAxis (axisNameY) < 0) {
-			Vector3 newScale = transform.localScale;
-			//newScale.x = -1.0f;
-			transform.localScale = newScale;
-		} else if (Input.GetAxis (axisNameY) > 0) {
-			Vector3 newScale = transform.localScale;
-			//newScale.x = 1.0f;
-			transform.localScale = newScale;
-		}
-		transform.position += transform.up * Input.GetAxis (axisNameY) * SpeedY * Time.deltaTime;
-	}
+    void CheckMoveY()
+    {
+        anim.SetFloat("SpeedY", Input.GetAxis(axisNameY));
+        if (Input.GetAxis(axisNameY) < 0)
+        {
+            Vector3 newScale = transform.localScale;
+            //newScale.x = -1.0f;
+            transform.localScale = newScale;
+        } else if (Input.GetAxis(axisNameY) > 0)
+        {
+            Vector3 newScale = transform.localScale;
+            //newScale.x = 1.0f;
+            transform.localScale = newScale;
+        }
+        transform.position += transform.up * Input.GetAxis(axisNameY) * SpeedY * Time.deltaTime;
+    }
 
-	//Need to check if we collide on the background.
-	bool IsOnTheGround()
-	{
-		return true;
-	}
+    //Need to check if we collide on the background.
+    bool IsOnTheGround()
+    {
+        return true;
+    }
 
+    bool NoWallX()
+    {
+        return true;
+    }
 
+    bool NoWallY()
+    {
+        return true;
+    }
 }
+
+
+
+
+
+
---
+++ 2DRPG/Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject player;
+    public Transform spawner;
+
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        spawner = GameObject.FindGameObjectWithTag("Spawner").transform;
+        Vector3 position = new Vector3(spawner.position.x, spawner.position.y, player.transform.position.z);
+        player.transform.position = position;
+    }
+    
+    void Update()
+    {
+      //  player.transform.position = GameObject.FindGameObjectWithTag("Spawner").transform.position;    
+    }
+}
