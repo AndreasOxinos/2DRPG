91717866beff7ec32d631a9b6b7b9334b1061245
AndreasOxinos <oxinosa@hotmail.com>
2013-12-16 13:06:32 +0200

    Door
    Door added to go from 1 scene to the other

---
+++ 2DRPG/Assets/Scene_Scripts/Start_door.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Start_door : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Application.LoadLevel("0");
+    }
+}
